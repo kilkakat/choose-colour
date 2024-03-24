2667d2fa40d79d3f04d914046ebd142ad9a10179
kilkakat <kkilasoniya@gmail.com>
2024-03-25 00:59:03 +0200

    add a code

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import {useState} from "react"
 import './App.css';
 
 function App() {
-  const [chooseColor, setChooseColor] = useState("green");
-
-  const colorChange = (event) =>
-  setChooseColor(event.target.value);
-
-return (
+  const [chooseColor, setChooseColor] = useState("");
+  
+  const colorChange = (event) => {
+    setChooseColor(event.target.value)
+  };
+  
+  return (
   <div class="App">
-
+  
   <h1>Color Selector</h1>
-
-  <div class="color-selector">
-
-    <select class="dropdownButton" value={chooseColor} onChange={colorChange}>
-      <option value="green">GREEN</option>
-      <option value="yellow">YELLOW</option>
-      <option value="red">RED</option>
-    </select>
-
-    <div class="squareColor" style={{ backgroundColor: chooseColor }}></div>
-  </div>
-</div>
-);
-}
-
-export default App;
+  
+      <div class="colorSelector">
+  
+        <select class="dropdownButton" value={chooseColor} onChange={colorChange}>
+         
+          <option value="">Choose Here!</option>
+          <option value="green">GREEN</option>
+          <option value="red">RED</option>
+          <option value="yellow">YELLOW</option>
+  
+        </select>
+  
+        <div class="square" style={{backgroundColor: chooseColor}}></div>
+  
+      </div>
+    </div>
+  )
+  }
+  
+  export default App;
+  
