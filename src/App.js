bf746f2b18f636a7bcb1344b61f710066c3334c3
kilkakat <kkilasoniya@gmail.com>
2024-04-01 13:58:01 +0300

    manage URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import './App.css';
 
 function App() {
   const [chooseColor, setChooseColor] = useState("");
-  
+  // const [query, setQuery] = useState('');
+
+  useEffect(() => {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const colorParam = urlParams.get('color');
+
+    if (colorParam && ['red', 'green', 'yellow'].includes(colorParam)) {
+      setChooseColor(colorParam);
+    }
+  }, []);
+
+
   const colorChange = (event) => {
-    setChooseColor(event.target.value)
+    const newColor = event.target.value;
+    
+    setChooseColor(newColor);
+    const newUrlParams = new URLSearchParams();
+    newUrlParams.set('color', setChooseColor);
+    window.history.pushState({newUrlParams});
   };
   
   return (
@@ -26,6 +43,8 @@ function App() {
   
         <div class="square" style={{backgroundColor: chooseColor}}></div>
   
+        {/* <h1>{query}</h1> */}
+
       </div>
     </div>
   )
